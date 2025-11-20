5ab54410748c7cc8faeb330b726f5c3a7068d1a7
omkarg419 <68603231+omkarg419@users.noreply.github.com>
2025-11-20 15:52:55 +0530

    feat: add custom rendering logic and initial HTML structure

---
+++ customReact/customReact.js
@@ -0,0 +1,31 @@
+function customRender(reactElement, mainConatainer) {
+  //   const domElement = document.createElement(reactElement.type);
+  //   domElement.innerHTML = reactElement.children;
+  //   domElement.setAttribute("href", reactElement.props.href);
+  //   domElement.setAttribute("target", reactElement.props.target);
+
+  //   mainConatainer.appendChild(domElement);
+
+  const domElement = document.createElement(reactElement.type);
+  domElement.innerHTML = reactElement.children;
+
+  for (const prop in reactElement.props) {
+    if (prop === "children") continue;
+    domElement.setAttribute(prop, reactElement.props[prop]);
+  }
+
+  mainConatainer.appendChild(domElement);
+}
+
+const reactElement = {
+  type: "a",
+  props: {
+    href: "https://www.google.com/",
+    target: "_blank",
+  },
+  children: "Click me to visit google",
+};
+
+const mainConatainer = document.querySelector("#root");
+
+customRender(reactElement, mainConatainer);
