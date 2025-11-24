194ee57f41b855b1de3924963fe75db4c2d0f035
omkarg419 <68603231+omkarg419@users.noreply.github.com>
2025-11-24 17:04:13 +0530

    feat: implement password generator UI with input and copy functionality

--- 05passwordgenerator/src/App.jsx
+++ 05passwordgenerator/src/App.jsx
@@ -4,12 +4,27 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [length, setLength] = useState(10);
+  const [numberAllowed, setNumberAllowed] = useState(false);
+  const [charAllowed, setCharAllowed] = useState(false);
+  const [password, setPassword] = useState("");
 
   return (
-    <>
-      <h1 className="bg-red-100 p-4">Testing perposes</h1>
-    </>
+    <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-700  text-yellow-400">
+      <h1 className="text-white my-2 text-center">Password Generator</h1>
+      <div className="mb-4 flex shadow rounded-lg  overflow-hidden ">
+        <input
+          type="text"
+          value={password}
+          className="outline-none w-full py-1 px-3 bg-white text-black"
+          placeholder="password"
+          readOnly
+        />
+        <button className="outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0  ">
+          Copy
+        </button>
+      </div>
+    </div>
   );
 }
 
