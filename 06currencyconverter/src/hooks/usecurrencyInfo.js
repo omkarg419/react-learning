22b284cec22f61cd6356fd3febe5e9013e9c6cb1
omkarg419 <68603231+omkarg419@users.noreply.github.com>
2025-11-25 13:48:56 +0530

    feat: add useCurrencyInfo hook for fetching currency data

---
+++ 06currencyconverter/src/hooks/usecurrencyInfo.js
@@ -0,0 +1,15 @@
+import { useState, useEffect } from "react";
+
+function useCurrencyInfo(currency) {
+  const [data, setData] = useState({});
+  useEffect(() => {
+    fetch(`https://open.er-api.com/v6/latest/${currency}`)
+      .then((res) => res.json())
+      .then((res) => setData(res[currency]));
+  }, [currency]);
+
+  console.log("Currency Info Data:", data);
+  return data;
+}
+
+export default useCurrencyInfo;
