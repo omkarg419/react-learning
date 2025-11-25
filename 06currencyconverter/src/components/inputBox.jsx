31c7e95a0abcd468b7dff8c392a6c5b93b49925d
omkarg419 <68603231+omkarg419@users.noreply.github.com>
2025-11-25 13:32:15 +0530

    feat: enhance InputBox component with layout, input, and select functionality

--- 06currencyconverter/src/components/inputBox.jsx
+++ 06currencyconverter/src/components/inputBox.jsx
@@ -11,7 +11,39 @@ function InputBox({
   currencyDisabled = false,
   className = "",
 }) {
-  return <div className={`bg-white p-3 rounded-lg text-sm flex${className}`}></div>;
+  return (
+    <div className={`bg-white p-3 rounded-lg text-sm flex${className}`}>
+      <div className="w-1/2">
+        <label className="text-black/40 mb-2 inline-block">{label}</label>
+        <input
+          type="number"
+          value={amount}
+          placeholder="Amount"
+          onChange={(e) =>
+            onAmountChange && onAmountChange(Number(e.target.value))
+          }
+          disabled={amountDisabled}
+          className="w-full outline-none bg-transparent py-1.5"
+        />
+      </div>
+
+      <div className="w-1/2 flex flex-wrap justify-end text-right">
+        <p className="text-black/40 mb-2 w-full">Currency Type</p>
+        <select
+          value={selectedCurrency}
+          onChange={(e)=>onCurrencyChange && onCurrencyChange(e.target.value)}
+          disabled={currencyDisabled}
+          className="w-full outline-none bg-transparent py-1.5 rounded-lg  cursor-pointer"
+        >
+          {currencyOptions.map((option) => (
+            <option key={option} value={option}>
+              {option.toUpperCase()}
+            </option>
+          ))}
+        </select>
+      </div>
+    </div>
+  );
 }
 
 export default InputBox;
