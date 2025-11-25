4996bf333b564375d2b57329316628c834ee4f5b
omkarg419 <68603231+omkarg419@users.noreply.github.com>
2025-11-25 12:21:47 +0530

    refactor: remove unused styles and update background image in App component

--- 06currencyconverter/src/App.jsx
+++ 06currencyconverter/src/App.jsx
@@ -1,14 +1,15 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
+import bg from "./assets/bg.png";
 import "./App.css";
 
 function App() {
   return (
-    <div className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
-      style={{ backgroundImage: "url('/background.jpg')" }}>
-      <h1 className="bg-yellow-500 text-white p-4 rounded">hello </h1>
-    </div>
+    <div
+      className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
+      style={{ backgroundImage: `url(${bg})` }}
+    ></div>
   );
 }
 
