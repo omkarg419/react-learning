0ad01faaa880ea1f51c1d362b9c4311449f8656e
omkarg419 <68603231+omkarg419@users.noreply.github.com>
2025-12-11 15:56:33 +0530

    feat: add TodoForm and TodoItem components for managing todos

--- todocontextlocal/src/App.jsx
+++ todocontextlocal/src/App.jsx
@@ -3,6 +3,8 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import { TodoProvider } from "./contexts/index.js";
+import TodoForm from "./components/TodoForm.jsx";
+import TodoItem from "./components/TodoItem.jsx";
 
 function App() {
   const [todos, setTodos] = useState([]);
@@ -44,7 +46,25 @@ function App() {
     <TodoProvider
       value={{ todos, addTodo, updateTodo, toggleComplete, deleteTodo }}
     >
-      <h1 className="text-2xl bg-gray-900 text-amber-200 inline p-8">heloo</h1>
+      <div className="bg-[#172842] min-h-screen py-8">
+        <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+          <h1 className="text-2xl font-bold text-center mb-8 mt-2">
+            Manage Your Todos
+          </h1>
+          <div className="mb-4">
+            {/* Todo form goes here */}
+            <TodoForm />
+          </div>
+          <div className="flex flex-wrap gap-y-3">
+            {/*Loop and Add TodoItem here */}
+            {todos.map((todo) => (
+              <div key={todo.id} className="w-full">
+                <TodoItem todo={todo} />
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
     </TodoProvider>
   );
 }
