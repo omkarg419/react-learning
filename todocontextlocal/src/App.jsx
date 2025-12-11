5708e628a0ff1bba6209b9cd45953de62101d5c1
omkarg419 <68603231+omkarg419@users.noreply.github.com>
2025-12-11 12:58:59 +0530

    refactor: wrap App component in TodoProvider and clean up formatting

--- todocontextlocal/src/App.jsx
+++ todocontextlocal/src/App.jsx
@@ -1,16 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { TodoProvider } from "./contexts/index.js";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <>
-     <h1 className='text-2xl bg-gray-900 text-amber-200 inline p-8'>heloo</h1>
-    </>
-  )
+    <TodoProvider
+      value={{ todos, addTodo, updateTodo, toggleComplete, deleteTodo }}
+    >
+      <h1 className="text-2xl bg-gray-900 text-amber-200 inline p-8">heloo</h1>
+    </TodoProvider>
+  );
 }
 
-export default App
+export default App;
